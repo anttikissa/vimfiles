578c89a6a87a03026fa59097493df59d310e84d8
U-hegel\antti <antti@hegel.(none)>
2009-10-19 15:56:10 +0300

    Add Windows installer script

---
+++ install.cmd
@@ -0,0 +1,10 @@
+
+@echo Moving your _vimrc out of the way
+move %USERPROFILE%\_vimrc %USERPROFILE%\_vimrc
+
+copy _vimrc %USERPROFILE%\_vimrc
+
+@echo Moving your _gvimrc out of the way
+move %USERPROFILE%\_gvimrc %USERPROFILE%\_gvimrc
+
+copy _gvimrc %USERPROFILE%\_gvimrc
